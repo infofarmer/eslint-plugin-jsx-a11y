accbf8077d7726c54fa351b1635cb6feff55d555
evcohen <124309035+evcohen@users.noreply.github.com>
2023-02-03 17:27:21 +0000

    Add files via upload

---
+++ flow/eslint.js
@@ -0,0 +1,29 @@
+/*
+ * @flow
+ */
+export type ESLintReport = {
+  node: any,
+  message: string,
+};
+
+export type ESLintSettings = {
+  [string]: mixed,
+  'jsx-a11y'?: {
+    components: {[string]: string},
+  },
+}
+
+export type ESLintContext = {
+  options: Array<Object>,
+  report: (ESLintReport) => void,
+  settings: ESLintSettings,
+};
+
+export type ESLintConfig = {
+  meta?: {[string]: mixed},
+  create: (context: ESLintContext) => mixed,
+}
+
+export type ESLintVisitorSelectorConfig = {
+  [string]: mixed,
+};
